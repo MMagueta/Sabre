80325471abfe4a5da6e590a1b465eef8375def68
Marcos Magueta <maguetamarcos@gmail.com>
2021-07-29 01:16:08 -0300

    feat: bootstrap with IL code sample

---
+++ Test/Program.fs
@@ -0,0 +1,7 @@
+open System
+open Sabre
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (Messages.Print())
+    0
---
+++ include/net5.0/coreassembly.h
@@ -0,0 +1,7 @@
+#define CORE_ASSEMBLY "System.Runtime"
+
+.assembly extern CORE_ASSEMBLY
+{
+  .publickeytoken = (B0 3F 5F 7F 11 D5 0A 3A )
+  .ver 4:0:0:0
+}
